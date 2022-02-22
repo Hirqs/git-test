2044857a4319797e5fd18b28705a5c2441a8e4e8
Hirqs <569543446@qq.com>
2022-02-22 10:46:08 +0800

    hot fix test conflict 1

--- src/main/java/com/rqs/git/GitTest.java
+++ src/main/java/com/rqs/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git4 hello hot-fix 2");
         System.out.println("hello hot-fix 1");
+        System.out.println("hot fix test conflict 1");
     }
 }
