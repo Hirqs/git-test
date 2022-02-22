0ffe790557d245f28c79f6e229d1f34eb8d83d85
Hirqs <569543446@qq.com>
2022-02-22 10:39:30 +0800

    hot fix 2

--- src/main/java/com/rqs/git/GitTest.java
+++ src/main/java/com/rqs/git/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
-        System.out.println("hello git4");
+        System.out.println("hello git4 hello hot-fix 2");
         System.out.println("hello hot-fix 1");
     }
 }
